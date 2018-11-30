2df9696a65997989f2282c7a4ff402081a8d9f86
Zyle <poonyapat.y@ku.th>
2018-11-30 12:35:57 +0700

    add firebase

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,36 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
+      <v-toolbar-title>
         <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-toolbar-items>
+        <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest">
+          vuetify
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
+  import Home from '@/views/Home'
+  import About from '@/views/About'
+  export default {
+    name: 'App',
+    components: {
+      Home,
+      About
+    },
+    data() {
+      return {
+        //
+      }
     }
   }
-}
 </script>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,21 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
+var config = {
+  apiKey: "AIzaSyDvuy00U7LJlDZ582Z8GKYdO9RfZ6BL24E",
+  authDomain: "poony-schedule-b9f47.firebaseapp.com",
+  databaseURL: "https://poony-schedule-b9f47.firebaseio.com",
+  projectId: "poony-schedule-b9f47",
+  storageBucket: "poony-schedule-b9f47.appspot.com",
+  messagingSenderId: "613530394495"
+};
+firebase.initializeApp(config);
+
+console.log(firebase.database())
 new Vue({
   router,
   store,
